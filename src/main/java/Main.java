89e355626e3ba1dc0c70346e3dae2cb0ddfd3898
luckydonald <work2.git@luckydonald.de>
2019-03-29 12:30:13 +0100

    Removed unneeded line.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,6 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) throws IOException {
         String val, valNoFormat, inputFileName;
-        String[] outputFileNames;
 
         inputFileName = "test.pdf";
 
